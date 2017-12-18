8531573f37f29a7f72f0b16b7623410b42f68857
Shamanoff <shamanoff@gmail.com>
2017-12-18 17:09:39 +0200

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ export const firebaseConfig = environment.firebaseConfig;
 import { AppComponent } from './app.component';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule
