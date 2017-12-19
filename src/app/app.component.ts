8805b6ebc0e2cf8a88c35ea79d52bc59952792f1
Shamanoff <shamanoff@gmail.com>
2017-12-19 15:31:26 +0200

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   constructor(private router: Router) {
   }
-
-
-  ngOnInit() {
-
-  }
-
-
 }
